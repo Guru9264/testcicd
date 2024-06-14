a0142bb166a684b419ee55ef8a22903415038ffc
Guruprasanna Tumkurkrishnamurthy <guruprasanna.tumkurkrishnamurthy@ust.com>
2024-06-14 21:08:33 +0000

    initial

--- app/listofrequestanditeminformation/webapp/Component.js
+++ app/listofrequestanditeminformation/webapp/Component.js
@@ -2,6 +2,7 @@ sap.ui.define(
     ["sap/fe/core/AppComponent"],
     function (Component) {
         "use strict";
+        
 
         return Component.extend("listofrequestanditeminformation.Component", {
             metadata: {
